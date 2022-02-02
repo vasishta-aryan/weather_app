c06502414c30f6a2c5124558c4aabb4995ec0f48
Aryan Vasishta <89566020+vasishta-aryan@users.noreply.github.com>
2022-02-02 10:24:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -27,22 +27,27 @@ const App = () => {
         onKeyPress={search}
       />
       {weather.main && (
-        <div className="city">
-          <h2 className="name">
-            <span>{weather.name}</span>
-            <sup>{weather.sys.country}</sup>
-          </h2>
-          <div className="temp">
-            {Math.round(weather.main.temp)}
-            <sup>&deg;C</sup>
-          </div>
-          <div className="info">
-            <img
-              className="icon"
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt={weather.weather[0].description}
-            />
-            <p>{weather.weather[0].description}</p>
+        <div className="container">
+          <div className="box">
+            <div className="card" id="city">
+              <h2 className="name">
+                <span>{weather.name}</span>
+                <sup>{weather.sys.country}</sup>
+              </h2>
+            </div>
+            <div className="card" id="temp">
+              {Math.round(weather.main.temp)}
+              <sup>&deg;C</sup>
+            </div>
+            <div className="card" id="info">
+              <img
+                className="icon"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+              />
+
+              <p>{weather.weather[0].description}</p>
+            </div>
           </div>
         </div>
       )}
